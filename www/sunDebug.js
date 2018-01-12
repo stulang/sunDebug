ba252d3ebc6fe13f2513ce21c653896cfc5e183a
stulang <sun92399@gmail.com>
2018-01-12 08:58:19 +0800

    Update sunDebug.js

--- www/sunDebug.js
+++ www/sunDebug.js
@@ -2,6 +2,9 @@ var exec = require('cordova/exec');
 var sunDebug   = {
   open:function (arg0, success, error) {
     exec(success, error, 'sunDebug', 'open', [arg0]);
-	}
+	},
+webDebugEnable:function (arg0, success, error) {
+                    exec(success, error, 'sunDebug', 'WebDebugEnable', [arg0]);
+                	}
 };
 module.exports = sunDebug;
